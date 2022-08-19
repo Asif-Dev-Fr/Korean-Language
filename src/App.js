5b766937ce262340f31af89ad26e63d53992c07c
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-08-19 17:57:44 +0200

    new page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import NavbarComponent from "./components/NavbarComponent";
 import Hangul from "./pages/hangul/Hangul";
+import HangulQuiz from "./pages/quizz/HangulQuiz";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/hangul" element={<Hangul />} />
+          <Route path="/quiz/hangul" element={<HangulQuiz />} />
+
         </Routes>
       </Router>
     </div>
