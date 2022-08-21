f5381925a4bf1a2d35301a9bc7fa7c5a605832b0
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-08-21 11:16:35 +0200

    homepage done

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className="container">
+      <div className="homepage">
+        <Link to="/hangul">
+          <Button variant="warning">Hangul List</Button>
+          <Button variant="success">Hangul Quiz</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
